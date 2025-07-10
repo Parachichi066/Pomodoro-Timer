1b6e7a46a968bf721ef564a1734d4bf9f2d80d58
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-07-10 09:47:54 +0100

    Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Timer from './pages/timer';
 import Navbar from './components/navBar';
 
